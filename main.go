a0ddf7d9c39fd5f4efea869d61069ad345b1b8fd
soloappsnmobile <solomon@appsnmobilesolutions.com>
2024-03-04 13:43:40 +0000

    initialized router

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bank-teller-backend/initializers"
+	"bank-teller-backend/routers"
 	"fmt"
+	"log"
+	"os"
 )
 
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectDatabase()
+}
+
 func main() {
-	fmt.Println("Hello, Wordddld!")
+	router := routers.SetupRouter()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router.Run(fmt.Sprintf(":%s", port))
 }
---
+++ routers/routers.go
@@ -0,0 +1,20 @@
+package routers
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	// Enable CORS
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
+	router.Use(cors.New(config))
+
+	return router
+
+}
